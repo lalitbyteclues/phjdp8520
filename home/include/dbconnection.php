1cbebe5c223a7c07edfdac35971d1aa2b64168d5
lalitbyteclues <lalit.sharma@byteclues.com>
2016-04-01 11:20:18 +0530

    Connection Update

--- home/include/dbconnection.php
+++ home/include/dbconnection.php
@@ -1,13 +1,13 @@
 <?php 
-$host        =    'localhost';
+/*$host        =    'localhost';
 $user        =    'root';
 $password    =    '';
-$database    =    'pharmerz';
-/*
+$database    =    'pharmerz';*/ 
+
 $host        =    'localhost';
 $user        =    'pharmerz';
 $password    =    'pharmerz@123';
-$database    =    'pharmerz';*/ 
+$database    =    'pharmerz';
 $conn        =    mysqli_connect($host,$user,$password) or die('Server Information is not Correct');
 mysqli_select_db($conn, $database) or die('Database Information is not correct');
 
